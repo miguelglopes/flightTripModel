315b93841b638414c1689ce6d5403362e2508180
Miguel Lopes <miguelll1991@gmail.com>
2020-01-14 16:30:14 +0000

    commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.OrTools.LinearSolver;
+
+
+namespace modeloViagensAviao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Viagens viagens = new Viagens();
+
+            //Solver solver = Solver.CreateSolver("realFever", "GLOP_LINEAR_PROGRAMMING");
+            Solver solver = Solver.CreateSolver("realFever", "CBC_MIXED_INTEGER_PROGRAMMING");
+
+            #region DECISIONS
+            foreach (Viagem v in viagens)
+                v.variable = solver.MakeBoolVar(v.id);
+            #endregion
+
+            #region GOALS
+            Objective objective = solver.Objective();
+            foreach (Viagem v in viagens)
+                objective.SetCoefficient(v.variable, v.price);
+            objective.SetMinimization();
+            #endregion
+        }
+    }
+}
---
+++ Viagem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.OrTools.LinearSolver;
+
+namespace modeloViagensAviao
+{
+    class Viagem
+    {
+        String from = null; 
+        String to = null;
+        DateTime date = new DateTime();
+        public Double price = 0;
+        int weekday = 0;
+        public Variable variable = null;
+
+        public string id
+        {
+            get { return from + "-" + to + "_" + date; }
+        }
+
+    }
+
+    class Viagens : List<Viagem>
+    {
+
+    }
+}
